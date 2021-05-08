6ee00b0c09a01037d50142ec92c6407d0b60b3e9
Le Hai <mskumji5@gmail.com>
2021-05-08 16:57:41 +0700

    fix docker local

--- tools/create-site.sh
+++ tools/create-site.sh
@@ -2,8 +2,7 @@
 read -p "Site folder name: " NAME
 
 read -p "MYSQL root password: " MYSQL_ROOT_PASSWORD
-read -p "MYSQL username: " MYSQL_USER
-read -p "MYSQL password: " MYSQL_PASSWORD
+
 read -p "MYSQL database: " MYSQL_DATABASE
 
 echo "Create folder code: "
@@ -12,4 +11,3 @@ docker exec -i nginx bash -c "mkdir -vp '/var/www/html/${NAME}'"
 
 echo "Create database: ${MYSQL_DATABASE}"
 docker exec mariadb mysql -uroot -p${MYSQL_ROOT_PASSWORD} -e "CREATE DATABASE IF NOT EXISTS ${MYSQL_DATABASE} ;
-GRANT ALL PRIVILEGES ON ${MYSQL_DATABASE}.* TO '${MYSQL_USER}'@'%' IDENTIFIED BY '${MYSQL_PASSWORD}';"
